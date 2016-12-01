7b7a687af86ef4efb55c29527e9406d29b40fb02
Stephannie Nolasco <ssnolasco@gmail.com>
2016-12-01 02:02:32 -0200

    Add leitura QRCode, Envio de pedido(Ajax), vibration

--- www/js/app.js
+++ www/js/app.js
@@ -15,10 +15,10 @@ $('#confirmar').on('click', function() {
 		var produto = this.firstChild.textContent;
 		var quantidade = this.lastChild.textContent;
 
-		texto += produto + ': ' + quantidade + ', ';
+		texto += '<blockquote>' + produto + ': ' + quantidade + ';</blockquote>';
 	});
 
-	$('#resumo').text(texto);
+	$('#resumo').html(texto);
 });
 
 $('.modal-trigger').leanModal();
@@ -31,4 +31,47 @@ $('.collection').on('click', '.badge', function() {
 $('.acao-limpar').on('click', function() {
 	$('#numero-mesa').val('');
 	$('.badge').remove();
+});
+
+$('.scan-qrcode').click(function() {
+	cordova.plugins.barcodeScanner.scan(
+      function (result) {
+      	if (result.text) {
+        	Materialize.toast('Mesa ' + result.text, 2000);
+      		$('#numero-mesa').val(result.text);
+      	}
+      },
+      function (error) {
+          Materialize.toast('Erro ' + error, 2000, 'red-text');
+      },
+      {
+          "preferFrontCamera" : false, // iOS and Android
+          "showFlipCameraButton" : false, // iOS and Android
+          "orientation" : "portrait" 
+          // Android only (portrait|landscape), default unset so it rotates with the device
+      }
+   );
+});
+
+$('.acao-finalizar').click(function() {
+	$.ajax({
+		url: 'http://cozinhapp.sergiolopes.org/novo-pedido',
+		data: {
+			mesa: $('#numero-mesa').val(),
+			pedido: $('#resumo').text() 
+		},
+	})
+	.done(function(result) {
+		Materialize.toast(result, 2000);
+		navigator.vibrate(200);
+		$('#numero-mesa').val('');
+		$('.badge').remove();
+	})
+	.fail(function(error) {
+		Materialize.toast(error, 3000, 'red-text');
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	
 });
